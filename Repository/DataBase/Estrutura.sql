0daf5cdae1b341b2b0a7ff254dfd364590f50c5a
andrehinckel <andrehinckel@gmail.com>
2019-06-28 17:26:37 -0300

    Criado a estrutura.

--- Repository/DataBase/Estrutura.sql
+++ Repository/DataBase/Estrutura.sql
@@ -1,1 +1,17 @@
-﻿
+﻿CREATE TABLE contabilidades(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(45)
+);
+CREATE TABLE clientes(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	id_contabilidade INT NOT NULL,
+	nome VARCHAR(45),
+	cpf VARCHAR(14)
+);
+CREATE TABLE usuarios(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	login VARCHAR(45),
+	senha VARCHAR(45),
+	data_nascimento DATETIME2,
+	id_contabilidade INT
+);
