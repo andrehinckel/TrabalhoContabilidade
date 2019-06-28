4c02ad1b4b972807b2dd78a6425f50e0d5033fe2
andrehinckel <andrehinckel@gmail.com>
2019-06-28 17:03:33 -0300

    Criado a classe de conexao e adicionado as referências

---
+++ Repository/DataBase/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DataBase
+{
+    public class Connection
+    {
+        public static SqlCommand OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            return command;
+        }
+    }
+}
