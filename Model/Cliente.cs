7d7c1863d0ead17fa44a63f9a4794bddd5f9c839
EduardoGabrielWehrmeister <dudu245@live.com>
2019-06-28 17:22:08 -0300

    Feito a Classe de Cliente

---
+++ Model/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cliente
+    {
+        public int Id;
+        public int IdContabilidade;
+        public Contabilidade Contabilidade;
+        public string Nome;
+        public string Cpf;
+    }
+}
