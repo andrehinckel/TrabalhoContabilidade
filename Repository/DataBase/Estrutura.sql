655c312c2a510d8650da72138c270a4a4f260c8b
andrehinckel <andrehinckel@gmail.com>
2019-07-01 16:14:53 -0300

    Terminada a criação da DataBase

--- Repository/DataBase/Estrutura.sql
+++ Repository/DataBase/Estrutura.sql
@@ -16,10 +16,41 @@ CREATE TABLE usuarios(
 	id_contabilidade INT
 );
 
+CREATE TABLE categorias(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(45)
+);
+
 CREATE TABLE contas_pagar(
 	id INT PRIMARY KEY IDENTITY(1,1),
+	id_cliente INT,
+	id_categorai INT,
+	nome VARCHAR(45),
+	data_vencimento DATETIME2,
+	data_pagamento DATETIME2,
+	valor DECIMAL(10,2)
 );
 
 CREATE TABLE contas_receber(
-	id INT PRIMARY KEY IDENTITY(1,1)
+	id INT PRIMARY KEY IDENTITY(1,1),
+	id_cliente INT,
+	id_cateoria INT,
+	nome VARCHAR(45),
+	data_pagamento DATETIME2,
+	valor DECIMAL(10,2)
+);
+
+CREATE TABLE cartoes_credito(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	id_cliente INT,
+	numero VARCHAR(45),
+	data_vencimento DATETIME2,
+	cvv VARCHAR(45)
+);
+
+CREATE TABLE compras(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	id_cartao_credito INT,
+	valor DECIMAL(10,2),
+	data_compra DATETIME2
 );
