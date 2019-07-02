25cce6e579cd3b4f9053afd6cff196153b0a0676
andrehinckel <andrehinckel@gmail.com>
2019-07-02 14:33:15 -0300

    Recriado a DB e criado o Controller Contabilidade

---
+++ View/Controllers/ContaPagarController.cs
@@ -0,0 +1,20 @@
+﻿using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class ContaPagarController : Controller
+    {
+        private ContaPagarRepository repository;
+
+        // GET: ContaPagar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
