b4fb236f1178cbe76f4752a10fd3915977966779
Angel-Costa <50925345+Angel-Costa@users.noreply.github.com>
2019-07-02 14:31:52 -0300

    Interface e Repositóriio do Contabilidade concluído!

--- Repository/Repositories/ContabilidadeRepository.cs
+++ Repository/Repositories/ContabilidadeRepository.cs
@@ -1,7 +1,10 @@
 ﻿using Model;
+using Repository.DataBase;
 using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +13,77 @@ namespace Repository.Repositories
 {
     public class ContabilidadeRepository : IContabilidadeRepository
     {
-        public bool Alterar(Contabilidade contabilidade)
+        public bool Delete(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Apagar(int id)
-        {
-            throw new NotImplementedException();
+            SqlCommand command = Connection.OpenConnection();
+            command.CommandText = "DELETE FROM contabilidades WHERE id = @ID";
+            command.Parameters.AddWithValue("@ID", id);
+            int quantidadeAfetada = command.ExecuteNonQuery();
+            command.Connection.Close();
+            return quantidadeAfetada == 1;
         }
 
         public int Inserir(Contabilidade contabilidade)
         {
-            throw new NotImplementedException();
+            SqlCommand command = Connection.OpenConnection();
+            command.CommandText = "INSERT INTO contabilidades (nome) OUTPUT INSERTED.ID VALUES (@NOME)";
+            command.Parameters.AddWithValue("@NOME", contabilidade.Nome);
+            int id = Convert.ToInt32(command.ExecuteScalar());
+            command.Connection.Close();
+            return id;
         }
 
         public Contabilidade ObterPeloId(int id)
         {
-            throw new NotImplementedException();
+            SqlCommand command = Connection.OpenConnection();
+            command.CommandText = @"SELECT * FROM contabilidades WHERE id = @ID";
+            DataTable table = new DataTable();
+            table.Load(command.ExecuteReader());
+            command.Connection.Close();
+            if (table.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            DataRow row = table.Rows[0];
+            Contabilidade contabilidade = new Contabilidade();
+            contabilidade.Id = Convert.ToInt32(row["id"]);
+            contabilidade.Nome = row["nome"].ToString();
+            return contabilidade;
         }
 
         public List<Contabilidade> ObterTodos()
         {
-            throw new NotImplementedException();
+            SqlCommand command = Connection.OpenConnection();
+            command.CommandText = "SELECT * FROM contabilidades";
+
+            DataTable table = new DataTable();
+            table.Load(command.ExecuteReader());
+            List<Contabilidade> contabilidades = new List<Contabilidade>();
+            command.Connection.Close();
+            foreach (DataRow row in table.Rows)
+            {
+                Contabilidade contabilidade = new Contabilidade()
+                {
+                    Id = Convert.ToInt32(row["id"]),
+                    Nome = row["nome"].ToString()
+                };
+
+                contabilidades.Add(contabilidade);
+
+            }
+            return contabilidades;
+        }
+
+        public bool Update(Contabilidade contabilidade)
+        {
+            SqlCommand command = Connection.OpenConnection();
+            command.CommandText = "UPDATE contabilidades SET nome = @NOME WHERE id = @ID";
+            command.Parameters.AddWithValue("@NOME", contabilidade.Nome);
+            command.Parameters.AddWithValue("@ID", contabilidade.Id);
+            int quantidadeAfetada = command.ExecuteNonQuery();
+            command.Connection.Close();
+            return quantidadeAfetada == 1;
         }
     }
 }
