e541f575b56f9dc596a9a4a0877694c765cc9e93
EduardoGabrielWehrmeister <dudu245@live.com>
2019-07-02 17:02:28 -0300

    Criado a Classe, Interface e Repository de Cartao de Credito,

--- View/Controllers/ContaReceberController.cs
+++ View/Controllers/ContaReceberController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,37 @@ namespace View.Controllers
 {
     public class ContaReceberController : Controller
     {
+        private ContaReceberRepository repositorio;
+
+        public ContaReceberController()
+        {
+            repositorio = new ContaReceberRepository();
+        }
+
         // GET: ContaReceber
         public ActionResult Index()
         {
+            List<ContaReceber> contasReceber = repositorio.ObterTodos();
+            ViewBag.ContasReceber = contasReceber;
             return View();
         }
+
+        public ActionResult Cadastro()
+        {
+            CategoriaRepository categoriaRepository = new CategoriaRepository();
+            List<Categoria> categorias = categoriaRepository.ObterTodos();
+            ViewBag.Categorias = categorias;
+
+            ClienteRepository clienteRepository = new ClienteRepository();
+            List<Cliente> clientes = clienteRepository.ObterTodos();
+            ViewBag.Clientes = clientes;
+
+            return View();
+        }
+
+        /*public ActionResult Store(int idCategoria, int idCliente, string nome, DateTime data_pagamento decimal valor)
+        {
+
+        }*/
     }
 }
