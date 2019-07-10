b50590eb7f552ad4ae33585f496ed6d116b451d0
André Hinckel <andrehinckel@gmail.com>
2019-07-10 14:39:33 -0300

    Recriado as tabelas para aumentar o valor aceito. Arrumado os scripts e adicionado script e mask para Valor.

--- Repository/DataBase/Estrutura.sql
+++ Repository/DataBase/Estrutura.sql
@@ -21,7 +21,7 @@ CREATE TABLE categorias(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR(45)
 );
-
+DROP TABLE contas_pagar;
 CREATE TABLE contas_pagar(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	id_cliente INT,
@@ -29,7 +29,7 @@ CREATE TABLE contas_pagar(
 	nome VARCHAR(45),
 	data_vencimento DATETIME2,
 	data_pagamento DATETIME2,
-	valor DECIMAL(10,2)
+	valor DECIMAL(14,2)
 );
 DROP TABLE contas_receber;
 CREATE TABLE contas_receber(
@@ -40,19 +40,19 @@ CREATE TABLE contas_receber(
 	data_pagamento DATETIME2,
 	valor DECIMAL(14,2)
 );
-
+DROP TABLE cartoes_credito;
 CREATE TABLE cartoes_credito(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	id_cliente INT,
 	numero VARCHAR(45),
 	data_vencimento DATETIME2,
-	cvv VARCHAR(45)
+	cvv VARCHAR(3)
 );
-
+DROP TABLE compras;
 CREATE TABLE compras(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	id_cartao_credito INT,
-	valor DECIMAL(10,2),
+	valor DECIMAL(14,2),
 	data_compra DATETIME2
 );
 
